119d955b66013dcbc9c636ecaa19bfbefaf35db6
Tanay Toshniwal <Tanaytoshniwal98@gmail.com>
2017-10-19 14:08:18 +0530

    Generating QR-Code

--- src/Generator.java
+++ src/Generator.java
@@ -1,12 +1,28 @@
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics2D;
 import java.awt.Label;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+
+import javax.imageio.ImageIO;
+import javax.swing.JOptionPane;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.ByteMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class Generator extends Frame implements ActionListener{
 	Label lbl;
@@ -26,6 +42,8 @@ public class Generator extends Frame implements ActionListener{
 		setLayout(null);
 		lbl=new Label("Input Text Here:");
 		txt=new TextField();
+		txt.setBackground(Color.GRAY);
+		txt.setForeground(Color.BLACK);
 		setBackground(Color.BLACK);
 		setForeground(Color.WHITE);
 		btn1=new Button("Generate");
@@ -53,11 +71,46 @@ public class Generator extends Frame implements ActionListener{
 				break;
 			}
 			case "Generate":{
-				String str=txt.getText();
-				
+				String qrCodeText = txt.getText();
+				int size = 125;
+				String fileType = "png";
+				try{
+					createQRImage(qrCodeText, size, fileType);
+				}
+				catch(IOException exp){
+					exp.printStackTrace();
+				}
+				catch(WriterException exp){
+					exp.printStackTrace();
+				}
+				JOptionPane.showMessageDialog(this, "Your QR-Code is Generated with text"+txt.getText(),"Congratulations",JOptionPane.INFORMATION_MESSAGE);
 				break;
 			}
 		}
+	}
+	public void createQRImage(String qrCodeText,int size,String fileType) throws IOException,WriterException{
+		String filePath = "T:\\WorkSpace\\eclipse1\\QR-code-Generator\\src";
+		File qrFile = new File(filePath);
+		Hashtable hintMap=new Hashtable();
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+		QRCodeWriter qrCodeWriter = new QRCodeWriter();
+		ByteMatrix byteMatrix = qrCodeWriter.encode(qrCodeText,BarcodeFormat.QR_CODE, size, size, hintMap);
+		int matrixWidth = byteMatrix.getWidth();
+		BufferedImage image = new BufferedImage(matrixWidth, matrixWidth,BufferedImage.TYPE_INT_RGB);
+		image.createGraphics();
+		Graphics2D graphics = (Graphics2D) image.getGraphics();
+		graphics.setColor(Color.WHITE);
+		graphics.fillRect(0, 0, matrixWidth, matrixWidth);
+		graphics.setColor(Color.BLACK);
+		for (int i = 0; i < matrixWidth; i++) {
+			for (int j = 0; j < matrixWidth; j++) {
+				if (byteMatrix.get(i,j)==0) {
+					graphics.fillRect(i, j, 1, 1);
+				}
+				//System.out.println(byteMatrix.get(i,j));
+			}
+		}
+		ImageIO.write(image, fileType, qrFile);
 	}
 	public static void main(String[]  args){
 		new Generator("QR Code Generator").setVisible(true);
